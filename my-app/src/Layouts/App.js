841e72de0b2179efd9275ab1a9b372b3dd0bbc88
sirsaumon <dragonkhanx@gmail.com>
2020-07-15 14:18:15 +0200

    liste

--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -3,6 +3,51 @@ import Item from '../components/Item';
 import List from '../components/List';
 
 const App = () => {
+    
+constlists = [
+    {
+        title:'Liste 1',
+        items: [
+            {
+                title:'item1',
+            },
+            {
+                title:'item2',
+            },
+            {
+                title:'item3',
+            },
+        ]
+    },
+    {
+        title:'Liste 2',
+        items: [
+        {
+            title:'item1',
+        },
+        {
+            title:'item2',
+        },
+        {
+            title:'item3',
+        },
+        ]
+    },
+    {
+        title:'Liste 3',
+        items: [
+        {
+            title:'item1',
+        },
+        {
+            title:'item2',
+        },
+        {
+            title:'item3',
+        },
+        ]
+    }
+    ];
 
     return (
         <div >
