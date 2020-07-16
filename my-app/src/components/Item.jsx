5879b900481d754d829fa9ae3ed8252e6ce6197a
Sileina <49021819+Sileina@users.noreply.github.com>
2020-07-16 11:13:25 +0200

    Exo 3

--- my-app/src/components/Item.jsx
+++ my-app/src/components/Item.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Item = ({ title }) => {
+    const [done, toggleDone] = useState(false)
+
+    const changeItemState = () => {
+        if (done === true)
+            toggleDone(false)
+        else
+            toggleDone(true)
+    }
+
+    let itemDoneClassName = done === true ? 'item item-done' : 'item'
     return (
-        <div className={"item"}>
+        <div className={itemDoneClassName} onClick={changeItemState}>
             {title}
         </div>
     )
