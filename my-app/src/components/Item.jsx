760e76b1ef159f49f787ad4d0feac4230c99d461
bastienlarrouture <44396321+bastienlarrouture@users.noreply.github.com>
2020-07-16 15:00:17 +0200

    CheckBox

--- my-app/src/components/Item.jsx
+++ my-app/src/components/Item.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
+import Checkbox from '@material-ui/core/Checkbox';
 
 // créer un état par le hooks d'état (useState) actif non actif
 // quand on clique sur l'item on change l'état
 // selon l'état on applique une class css actif ou non actif
 
 const Item = ({ title }) => {
-    const [check, setCheck] = useState(false)
+    const [checkBoxVal, setMyCheckbox] = useState(true)
+
+    const toggleCheck = () => {
+        checkBoxVal ? setMyCheckbox(false) : setMyCheckbox(true)
+        console.log(checkBoxVal)
+    }
 
     return (
-        <div >
+        <div onChange={toggleCheck}>
             {title}
+            <Checkbox></Checkbox>
         </div>
     )
 }
-
 
 export default Item
