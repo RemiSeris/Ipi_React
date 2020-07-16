0ef7ba305b587cbbde6f34e01613c5c2076848c7
sirsaumon <dragonkhanx@gmail.com>
2020-07-16 09:56:55 +0200

    commitbeforecorrection

--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Item from '../components/Item';
 import List from '../components/List';
 
-const App = () => {
-    
 constlists = [
     {
         title:'Liste 1',
@@ -49,9 +47,32 @@ constlists = [
     }
     ];
 
+    const defaultList
+
+const App = () => {
+
+    const [myLists, setMyList] = useState(lists)
+    const changeTitle = () => {
+
+    }
+
+    const addList = () => {
+        myLists.push({defaultList})
+        setMyList(myLists.map(list => list))
+    }
+
+    const removeList = () => {
+        myLists.pop()
+        setMyList(MyLists.map(list => list))
+    }
+
     return (
-        <div >
+        <div>
             HELLO WORLD
+            myLists.map({items, title}) =>
+            <List title={title}>
+            </List>
+
             <List title={"titre de ma liste"} classname='list'>
                 <Item title={"Hello World"}/>
                 <Item title={"Hello World"}/>
@@ -59,6 +80,8 @@ constlists = [
                 <Item title={"Hello World"}/>
                 <Item title={"Hello World"}/>
             </List>
+            <Button onClick={changeTitle} title="Add"></Button>
+            <Button onClick={changeTitle} title="Delete"></Button>
         </div>
         )
 }
--- my-app/src/components/Button.jsx
+++ my-app/src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = () => {
+const Button = (onClick, title) => {
     return(
-        <button>Bouton</button>
+        <div><button onClick={onClick}>{title}</button></div>
     )
 }
 
