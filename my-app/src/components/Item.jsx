84ae812072e74e3f39bf2b1c140d0d908f44ae02
remi seris <remiseris@gmail.com>
2020-07-16 11:14:09 +0200

    correction exo3

--- my-app/src/components/Item.jsx
+++ my-app/src/components/Item.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// créer un état par le hooks d'état (useState) actif non actif
+// quand on clique sur l'item on change l'état
+// selon l'état on applique une class css actif ou non actif
 
 const Item = ({ title }) => {
+    const [done, toogleDone] = useState(false)
+
+
+
+    const changeItemState = () => {
+        //si l'item est a  done true on le passe à done false
+        if (done === true)
+            toogleDone(false)
+        else
+            toogleDone(true)
+
+        //si l'item est à done false, on le passe à donne true
+    }
+
+    console.log('is Item Done ?', done)
+
+    // condition ? si ma condition est rempli : si ma condition n'est pas remlie
+
+    // const itemClassName = done === false ? "item" : "item itemDone"
+
+    let itemClassName = "item"
+
+    if (done === true)
+        itemClassName = "item itemDone"
+
+
     return (
-        <div className={"item"}>
+        <div className={itemClassName} onClick={changeItemState}>
             {title}
         </div>
     )
