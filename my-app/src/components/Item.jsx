53bfcea37103637530bfd4e4979fdc9cfcd1479e
NYAZAK <zakaria.nya@hetic.net>
2020-07-16 14:50:26 +0200

    instructions 2 exercice sur la checkbox

--- my-app/src/components/Item.jsx
+++ my-app/src/components/Item.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Checkbox } from '@material-ui/core';
 
 // créer un état par le hooks d'état (useState) actif non actif
 // quand on clique sur l'item on change l'état
@@ -6,10 +7,20 @@ import React, { useState } from 'react'
 
 const Item = ({ title }) => {
     const [check, setCheck] = useState(false)
+    
+  const handleChange = (event) => {
+    setCheck(event.target.checked);
+  };
+
+  console.log('is checked', check);
 
     return (
         <div >
-            {title}
+            {`${title} `} <Checkbox
+        checked={check}
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
         </div>
     )
 }
