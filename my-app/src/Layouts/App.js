8e6bf026c02a465acf969e15a549d6a40157940b
Meital <to31@hotmail.fr>
2020-07-16 10:46:27 +0200

    exo switch

--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -1,8 +1,10 @@
 //Lorsque on veut créer un component on importe React
-import React, { useState } from 'react';
+import React, {useCallback, useState} from 'react';
 import List from '../components/List'
 import Item from '../components/Item'
-import Button from '../components/Button';
+import Switch from '@material-ui/core/Switch';
+import Button from "@material-ui/core/Button";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 //On simule un modéle de donée (tableau de list)
@@ -71,52 +73,62 @@ const defaultList = {
 const App = () => {
     // on utilise un hooks d'état pour pouvoir moifier la page
     // const [maVariable d'état, mon Setter de la variable] = useState(ma valeur initial)
-    const [myLists, setMyList] = useState(lists)
+    const [myLists, setMyList] = useState(lists);
+    const [switchValue, setSwitchValue] = useState(false);
 
 
     //on crée une fonction pour ajouter un élément à la liste
     const addList = () => {
-
         //on pousse un nouvelle élément dans le tableau de liste
         myLists.push(defaultList)
-
         // on crée une copie de notre tableau pour changer la référence 
         const listCpy = myLists.map(list => list)
-
         //on utilise le setter d'état pour changer l'états de nos liste
         setMyList(listCpy)
-    }
+    };
 
     //on crée une fonction pour enlever un élément à la liste
     const removeList = () => {
-
         //on enléve le dernier élément du tableau de list
         myLists.pop()
-
         // on crée une copie de notre tableau pour changer la référence 
         const listCpy = myLists.map(list => list)
-
         //on utilise le setter d'état pour changer l'états de nos liste
         setMyList(listCpy)
     }
 
+    const onSwitchChange = () =>{
+        setSwitchValue(!switchValue);
+    }
     return (
         <div className='layout'>
             {
                 // On utilise la méthode .map pour parcourir les éléments,
                 // de nos tableau et renvoyer pour chaque élément le component indiquée
-                myLists.map(({ items, title }) =>
-                    // On affiche nos lists une a une sous forme de component
-                    <List title={title}  >
-                        {
-                            // On affiche les items d'une liste une à une sous forme de component
-                            items.map(({ title: itemTitle }) => <Item title={itemTitle} />)
-                        }
-                    </List>
+                myLists.map(({items, title}) =>
+                    <div>
+                        <List title={title}>
+                            {
+                                // On affiche les items d'une liste une à une sous forme de component
+                                items.map(({title: itemTitle}) => <Item title={itemTitle}/>)
+                            }
+                        </List>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    checked={switchValue}
+                                    onChange={onSwitchChange()}
+                                    name="checkedB"
+                                    color="primary"
+                                />
+                            }
+                            label="Primary"
+                        />
+                    </div>// On affiche nos lists une a une sous forme de component
                 )}
             {/* On utilise notre component générique Button pour effectuer l'action d'ajout et de supression d'une liste dans le tableau de list*/}
-            <Button onClick={addList} title={'Add'} />
-            <Button onClick={removeList} title={'delete'} />
+            <Button onClick={addList} variant={'outlined'}>Add</Button>
+            <Button onClick={removeList} variant={'outlined'}>Delete</Button>
         </div>
     )
 }
