7ff281bc7f697100113469ecfcf74f468845dd98
romainkruz <romainkruszewski@gmail.com>
2020-07-16 15:37:34 +0200

    CheckBox

--- my-app/src/components/Item.jsx
+++ my-app/src/components/Item.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 // créer un état par le hooks d'état (useState) actif non actif
 // quand on clique sur l'item on change l'état
@@ -7,9 +9,26 @@ import React, { useState } from 'react'
 const Item = ({ title }) => {
     const [check, setCheck] = useState(false)
 
+    const boxCheck = () => {
+        // setChecked(!checked)
+        if (check === true)
+            setCheck(false)
+        else
+            setCheck(true)
+    }
+
     return (
         <div >
             {title}
+                  <FormControlLabel
+        control={
+          <Checkbox
+            check={check}
+            onChange={boxCheck}
+            color="primary"
+          />
+        }
+      />
         </div>
     )
 }
