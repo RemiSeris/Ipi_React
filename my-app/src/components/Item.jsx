8394b398974f36ba86040ca265c7aa8116b2d7b1
cla31 <clairerodriguez79@yahoo.fr>
2020-07-18 20:24:45 +0200

    exo instruction 2: item done, qd item cliqué on applique classe CSS et quand on reclique dessus on enlève la classe CSS

--- my-app/src/components/Item.jsx
+++ my-app/src/components/Item.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
+//créer un état actif non actif par le hooks d'état {useState}
+// quand on clique sur l'item on change l'état
+//selon l'état on applique une classe css active ou non active
 
 const Item = ({ title }) => {
+    const[done, toogleDone]=useState(false)
+
+    const changeItemState=() =>{
+        //si l'item est à done true on le passe à done false
+        if (done === true)
+            toogleDone(false)
+        //si l'item est à done false, on le passe à done true
+        else
+            toogleDone(true)
+    }
+
+
+    //equivalent d'une arrow fonction:
+    /*function changeItemState(){
+
+    }*/
+
+    console.log('is Item Done?',done)
+
+    //on passe la classe en variable:
+    //rappel, condition? si ma condition est remplie: si ma condition n'est pas remplie
+    const itemClassName=done === false?"item":"item itemDone"
+
+    //condition sans le mode ternaire:
+    /*let itemClassName="item"
+    if(done===true)
+        itemClassName="item itemDone"
+    */
+
     return (
-        <div className={"item"}>
+        <div className={itemClassName} onClick={changeItemState}>
             {title}
         </div>
     )
