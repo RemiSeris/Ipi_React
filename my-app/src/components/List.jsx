8aeffb05bcf222d454d995d6c4bffc3e7bd31a37
Meital <to31@hotmail.fr>
2020-07-21 16:40:40 +0200

    wip 6

--- my-app/src/components/List.jsx
+++ my-app/src/components/List.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import CreateIcon from '@material-ui/icons/Create';
 import Button from "@material-ui/core/Button";
+import {appContext} from "./appContextProvider";
 
 class List extends React.Component {
-    constructor(props){
+    static contextType = appContext;
+    constructor(props) {
         super(props);
     }
 
+
     render() {
+        const {myLists, setMyList} = this.context;
+
+        const addItem = () => {
+            myLists.forEach(x => {
+                if ( x.items === this.props){
+                    x.items.push({title : 'new item'});
+                }
+            });
+        };
+
         return (
             <div className={'list'}>
                 <div className="listTitle" style={{display: "flex"}}>
                     {this.props.title}
-                    <CreateIcon style={{marginLeft: 10, fontSize: 20 , cursor: "pointer" }} onClick={this.props.clickIcon} />
+                    <CreateIcon style={{marginLeft: 10, fontSize: 20, cursor: "pointer"}}
+                                onClick={this.props.clickIcon}/>
                 </div>
                 <div className={'listItems'}>
                     {this.props.children}
                 </div>
                 <div>
-                    <Button  variant={'outlined'} size={"medium"} style={{width : 25}}>Add</Button>
+                    <Button variant={'outlined'} size={"medium"} style={{width: 25}}
+                            onClick={addItem}>Add</Button>
                 </div>
             </div>
         )
