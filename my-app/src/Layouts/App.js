2e32df41ef05897d7bb1f294ac4fc4c3a990eafd
Zein1 <delavaloire.clement@gmail.com>
2020-07-21 11:37:01 +0200

    test
    test

--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 import List from '../components/List'
 import Item from '../components/Item'
 import Button from '@material-ui/core/Button'
+import { KEY_LOCAL_STORAGE } from '../const'
+import EffectExampl from '../components/EffectExampl';
+
 
 //On simule un modéle de donée (tableau de list)
 const lists = [
@@ -66,6 +69,11 @@ const defaultList = {
     ]
 }
 
+const testObject = {
+    data1: 'myData1',
+    data2: 2,
+}
+
 // on déclare un componet sous forme d'arrow funtion 
 const App = () => {
     // on utilise un hooks d'état pour pouvoir moifier la page
@@ -99,6 +107,12 @@ const App = () => {
         setMyList(listCpy)
     }
 
+
+
+    const saveList = () => {
+        localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(myLists))
+    }
+
     return (
         <div className='layout'>
             {
@@ -106,7 +120,7 @@ const App = () => {
                 // de nos tableau et renvoyer pour chaque élément le component indiquée
                 myLists.map(({ items, title }) =>
                     // On affiche nos lists une a une sous forme de component
-                    <List title={title}  >
+                    <List title={title} myProps={"zeaaze"} >
                         {
                             // On affiche les items d'une liste une à une sous forme de component
                             items.map(({ title: itemTitle }) => <Item title={itemTitle} />)
@@ -115,7 +129,7 @@ const App = () => {
                 )}
             {/* On utilise notre component générique Button pour effectuer l'action d'ajout et de supression d'une liste dans le tableau de list*/}
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <Button onClick={addList}>
+                <Button onClick={addList} variant='contained'>
                     {"ADD"}
                 </Button>
                 <Button onClick={removeList}>
@@ -127,6 +141,11 @@ const App = () => {
                 <Button onClick={removeList} title={'delete'} /> 
                 */}
             </div>
+
+            <Button onClick={saveList}>
+                {"Save list"}
+            </Button>
+            <EffectExampl />
         </div>
     )
 }
