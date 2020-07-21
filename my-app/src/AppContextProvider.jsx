c153829bac546b3a7184ab5d929ed6fbf6c92fb9
remi seris <remiseris@gmail.com>
2020-07-21 15:57:21 +0200

    wip

---
+++ my-app/src/AppContextProvider.jsx
@@ -0,0 +1,165 @@
+import React, { createContext, useState, useEffect } from 'react'
+import { KEY_LOCAL_STORAGE } from './const'
+
+
+//On simule un modéle de donée (tableau de list)
+const defaultLists = [
+    {
+        title: 'Liste 1',
+        items: [
+            {
+                title: 'item1',
+            },
+            {
+                title: 'item2',
+            },
+            {
+                title: 'item3',
+            },
+        ]
+    },
+    {
+        title: 'Liste 2',
+        items: [
+            {
+                title: 'item1',
+            },
+            {
+                title: 'item2',
+            },
+            {
+                title: 'item3',
+            },
+        ]
+    },
+    {
+        title: 'Liste 3',
+        items: [
+            {
+                title: 'item1',
+            },
+            {
+                title: 'item2',
+            },
+            {
+                title: 'item3',
+            },
+        ]
+    }
+]
+
+//onj déclare un template d'une list
+const defaultList = {
+    title: 'Nouvelle liste',
+    items: [
+        {
+            title: 'item1',
+        },
+        {
+            title: 'item2',
+        },
+        {
+            title: 'item3',
+        },
+    ]
+}
+
+
+
+export const AppContext = createContext({})
+
+
+const AppContextProvider = ({ children }) => {
+    // on utilise un hooks d'état pour pouvoir moifier la page
+    // const [maVariable d'état, mon Setter de la variable] = useState(ma valeur initial)
+    const [myLists, setMyList] = useState(defaultLists)
+
+
+    //On utilise un useEffect pour utiliser un effet
+    useEffect(() => {
+        const mydataFromStorage = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE))
+        if (mydataFromStorage)
+            setMyList(mydataFromStorage)
+    }, [])
+
+
+
+
+    useEffect(() => {
+        localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(myLists))
+    }, [myLists])
+
+    //on crée une fonction pour ajouter un élément à la liste
+    const addList = () => {
+
+        //on pousse un nouvelle élément dans le tableau de liste
+        myLists.push(defaultList)
+
+        // on crée une copie de notre tableau pour changer la référence 
+        const listCpy = myLists.map(list => list)
+
+        //on utilise le setter d'état pour changer l'états de nos liste
+        setMyList(listCpy)
+    }
+
+    //on crée une fonction pour enlever un élément à la liste
+    const removeList = () => {
+
+        //on enléve le dernier élément du tableau de list
+        myLists.pop()
+
+        // on crée une copie de notre tableau pour changer la référence 
+        const listCpy = myLists.map(list => list)
+
+        //on utilise le setter d'état pour changer l'états de nos liste
+        setMyList(listCpy)
+    }
+
+    //Je rajoute un item dans une list
+    // j'ai besoin de savoir dans quelle list je rajouite l'item
+    const addItem = (list) => {
+        myLists.forEach((myList) => {
+            //Si ma list est la list passer en argument
+            if (myList === list) {
+                myList.items.push({ title: 'new Item' })
+                //je rajoute un item à ma list
+            }
+        })
+        // on crée une copie de notre tableau pour changer la référence 
+        const listCpy = myLists.map(list => list)
+
+        //on utilise le setter d'état pour changer l'états de nos liste
+        setMyList(listCpy)
+    }
+
+    //j'enlève le dernier item d'une list
+    // j'ai besoin de savoir dans quelle list je rajouite l'item
+    const removeItem = (list) => {
+        myLists.forEach((myList) => {
+            //Si ma list est la list passer en argument
+            if (myList === list) {
+                //je supprime un item à ma list
+                myList.items.pop()
+            }
+        })
+        // on crée une copie de notre tableau pour changer la référence 
+        const listCpy = myLists.map(list => list)
+
+        //on utilise le setter d'état pour changer l'états de nos liste
+        setMyList(listCpy)
+    }
+
+    const value = {
+        myLists,
+        addList,
+        removeList,
+        addItem,
+        removeItem,
+    }
+
+    return <AppContext.Provider value={value}>
+        {children}
+    </AppContext.Provider>
+}
+
+export default AppContextProvider
--- my-app/src/Layouts/App.js
+++ my-app/src/Layouts/App.js
@@ -1,135 +1,29 @@
 //Lorsque on veut créer un component on importe React
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import List from '../components/List'
 import Item from '../components/Item'
 import Button from '@material-ui/core/Button'
-import { KEY_LOCAL_STORAGE } from '../const'
-import Counter1 from '../components/Counter1';
-import CounterContextProvider from '../CounterContextProvider'
-
-//On simule un modéle de donée (tableau de list)
-const defaultLists = [
-    {
-        title: 'Liste 1',
-        items: [
-            {
-                title: 'item1',
-            },
-            {
-                title: 'item2',
-            },
-            {
-                title: 'item3',
-            },
-        ]
-    },
-    {
-        title: 'Liste 2',
-        items: [
-            {
-                title: 'item1',
-            },
-            {
-                title: 'item2',
-            },
-            {
-                title: 'item3',
-            },
-        ]
-    },
-    {
-        title: 'Liste 3',
-        items: [
-            {
-                title: 'item1',
-            },
-            {
-                title: 'item2',
-            },
-            {
-                title: 'item3',
-            },
-        ]
-    }
-]
-
-//onj déclare un template d'une list
-const defaultList = {
-    title: 'Nouvelle liste',
-    items: [
-        {
-            title: 'item1',
-        },
-        {
-            title: 'item2',
-        },
-        {
-            title: 'item3',
-        },
-    ]
-}
-
+import { useContext } from 'react';
+import { AppContext } from '../AppContextProvider';
+import TextFieldExample from '../components/TextFieldExample'
 
 // on déclare un componet sous forme d'arrow funtion 
 const App = () => {
-    // on utilise un hooks d'état pour pouvoir moifier la page
-    // const [maVariable d'état, mon Setter de la variable] = useState(ma valeur initial)
-    const [myLists, setMyList] = useState(defaultLists)
-
-
-    //On utilise un useEffect pour utiliser un effet
-    useEffect(() => {
-        const mydataFromStorage = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE))
-        if (mydataFromStorage)
-            setMyList(mydataFromStorage)
-    }, [])
-
-
-
-
-    useEffect(() => {
-        localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(myLists))
-    }, [myLists])
-
-    //on crée une fonction pour ajouter un élément à la liste
-    const addList = () => {
-
-        //on pousse un nouvelle élément dans le tableau de liste
-        myLists.push(defaultList)
-
-        // on crée une copie de notre tableau pour changer la référence 
-        const listCpy = myLists.map(list => list)
-
-        //on utilise le setter d'état pour changer l'états de nos liste
-        setMyList(listCpy)
-    }
-
-    //on crée une fonction pour enlever un élément à la liste
-    const removeList = () => {
-
-        //on enléve le dernier élément du tableau de list
-        myLists.pop()
-
-        // on crée une copie de notre tableau pour changer la référence 
-        const listCpy = myLists.map(list => list)
-
-        //on utilise le setter d'état pour changer l'états de nos liste
-        setMyList(listCpy)
-    }
-
+    const { myLists, addList, removeList } = useContext(AppContext)
 
 
     return (
         <div className='layout'>
+            <TextFieldExample />
             {
                 // On utilise la méthode .map pour parcourir les éléments,
                 // de nos tableau et renvoyer pour chaque élément le component indiquée
-                myLists.map(({ items, title }) =>
+                myLists.map((list) =>
                     // On affiche nos lists une a une sous forme de component
-                    <List title={title} myProps={"zeaaze"} >
+                    <List list={list} >
                         {
                             // On affiche les items d'une liste une à une sous forme de component
-                            items.map(({ title: itemTitle }) => <Item title={itemTitle} />)
+                            list.items.map(({ title: itemTitle }) => <Item title={itemTitle} />)
                         }
                     </List>
                 )}
@@ -141,17 +35,9 @@ const App = () => {
                 <Button onClick={removeList}>
                     {"DELETE"}
                 </Button>
-                {/* 
-                <Button onClick={addList} title={'Add'} />
+            </div>
 
-                <Button onClick={removeList} title={'delete'} /> 
-                */}
-            </div>
-            <div>
-                <CounterContextProvider>
-                    <Counter1 />
-                </CounterContextProvider>
-            </div>
+
         </div>
     )
 }
