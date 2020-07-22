de4494b8b4bd824f1d7cc3a0d7b81d4184c51ca3
Quentin Baillau <quentin.baillau@cgi.com>
2020-07-22 09:02:13 +0200

    exos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,37 +5,14 @@ import React, { useState } from 'react'
 // selon l'état on applique une class css actif ou non actif
 
 const Item = ({ title }) => {
-    const [done, toogleDone] = useState(false)
-
-
-
-    const changeItemState = () => {
-        //si l'item est a  done true on le passe à done false
-        if (done === true)
-            toogleDone(false)
-        else
-            toogleDone(true)
-
-        //si l'item est à done false, on le passe à donne true
-    }
-
-    console.log('is Item Done ?', done)
-
-    // condition ? si ma condition est rempli : si ma condition n'est pas remlie
-
-    // const itemClassName = done === false ? "item" : "item itemDone"
-
-    let itemClassName = "item"
-
-    if (done === true)
-        itemClassName = "item itemDone"
-
+    const [check, setCheck] = useState(false)
 
     return (
-        <div className={itemClassName} onClick={changeItemState}>
+        <div >
             {title}
         </div>
     )
 }
+
 
 export default Item
