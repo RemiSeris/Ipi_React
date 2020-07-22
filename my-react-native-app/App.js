493ae673b96dc76641e9a6eb4e7cbded18adbd32
elya01 <44399210+elya01@users.noreply.github.com>
2020-07-22 15:28:29 +0200

    MAJ

--- my-react-native-app/App.js
+++ my-react-native-app/App.js
@@ -1,16 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useContext } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 import { AppContext } from './AppContextPovider';
+import Item from './components/Item'
+import List from './components/List'
 
 const App = () => {
   const { myLists } = useContext(AppContext)
 
-  return (
+  /* return (
     <View style={styles.container}>
       {myLists.map((list) => {
         return <Text>{list.title}</Text>
       })}
+      <StatusBar style="auto" />
+    </View>
+  );
+} */
+
+
+  return (
+    <View style={styles.container}>
+      {myLists.map((list) => {
+        return (
+          <View><Text>{list.title}</Text>
+            <List list={list} >
+              {list.items.map(({ title: itemTitle }) => <Item title={itemTitle} />)}
+
+            </List>
+
+          </View>
+        );
+      })}
+
+      <Button title={'ADD'}>  </Button>
+      <Button title={"DELETE "}>  </Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,7 +44,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- my-react-native-app/components/List.jsx
+++ my-react-native-app/components/List.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+import { AppContext } from '../AppContextPovider';
+
 
 
 const List = ({ list, children }) => {
+    const { removeItem, addItem } = useContext(AppContext)
+    const { title } = list
+    const newAddItem = () => {
+        addItem(list)
+    }
+
+    const newRemoveItem = (list) => {
+        removeItem(list)
+    }
     return (
         <View>
+            <Text>{list.title}</Text>
             {children}
+            <Button onPress={newAddItem} title="add Item"> </Button>
+            <Button onPress={newRemoveItem} title="add Item"> </Button>
         </View>
     )
 }
