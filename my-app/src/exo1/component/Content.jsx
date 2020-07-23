47fb408f6a5458eb54e5eeb79aa83cefffc5ad57
ManonCstx <castex.manon@gmail.com>
2020-07-23 14:47:13 +0200

    Exo1

---
+++ my-app/src/exo1/component/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Content = ({Content}) => {
+    return (
+        <div className={"Content"}>
+        {Content}
+        </div>
+    )
+}
+
+export default Content
---
+++ my-app/src/exo1/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = ({legalMention}) => {
+    return (
+        <div className={"Footer"}>
+        {legalMention}
+        </div>
+    )
+}
+
+export default Footer
