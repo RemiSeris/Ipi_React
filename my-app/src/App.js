fade56b1d1158b5317c8432fa68964e8e406a6aa
ManonCstx <castex.manon@gmail.com>
2020-07-23 15:47:32 +0200

    Exo3Et4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 //Lorsque on veut créer un component on importe React
 import React from 'react';
-import Exo1 from './exo1/Exo1';
+import Exo1 from './exo1/Exo1'
 import Exo2 from './exo2/Exo2';
+import Exo3 from './exo3/Exo3';
+import CounterContextProvider from './exo4/CounterContextProvider';
 // import Exo2 from './exo2/Exo2'
 // import Exo3 from './exo3/Exo3'
-// import Exo4 from './exo4/Exo4'
+import Exo4 from './exo4/Exo4'
 
 const App = () => {
 
@@ -12,6 +14,7 @@ const App = () => {
         <div className='layout'>
             {
                 <Exo1></Exo1>
+
                 /*
                  <Exo1/>
                 <Exo2/>
@@ -21,6 +24,12 @@ const App = () => {
             }
             {
                 <Exo2></Exo2>
+            }
+            {
+                <Exo3></Exo3>
+            }
+            {
+                    <Exo4></Exo4>
             }
         </div>
     )
--- my-app/src/exo3/Exo3.jsx
+++ my-app/src/exo3/Exo3.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import { TextField, Button } from '@material-ui/core'
+import React, { useEffect } from 'react'
+import { TextField, Button, InputAdornment } from '@material-ui/core'
+import EditIcon from '@material-ui/icons/Edit'
 import "./exo3.css"
+import { useState } from 'react'
+import SaveIcon from '@material-ui/icons/Save';
 
 const KEY_LOCAL_STORAGE = 'my_title'
 
 const Exo3 = () => {
+    const [inputValue, changeInput] = useState('')
+    const changeInputOnChange = (event) => {
+        changeInput(event.target.value)
+    }
+    const saveInput = () =>{
+        localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(inputValue))
+
+    }
+    useEffect(() => {
+        let loadedInput = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE))
+        if (loadedInput !== undefined && loadedInput !== null) {
+            changeInput(loadedInput)
+        }
+    }, [])
     return (
         <div className={"exo3"}>
+
+            <TextField value={inputValue} onChange={changeInputOnChange} id="input" label="TextField" InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <EditIcon />
+                        </InputAdornment>
+                    ),
+                }}/>
+                <div className='btn-submit'>
+                <Button onClick={saveInput} variant="contained" startIcon={<SaveIcon />}>Save</Button>
+                </div>            
         </div>
     )
 }
