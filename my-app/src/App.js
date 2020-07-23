43ff3f473c1318855137edeca93ea9af0ccbe1a8
romainkruz <romainkruszewski@gmail.com>
2020-07-23 16:28:04 +0200

    Exo 3-Bad

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Exo1 from './exo1/Exo1'
 import Exo2 from './exo2/Exo2'
-// import Exo3 from './exo3/Exo3'
+import Exo3 from './exo3/Exo3'
 // import Exo4 from './exo4/Exo4'
 
 const App = () => {
@@ -13,10 +13,10 @@ const App = () => {
                 
                 /*
                 <Exo1/>
-                */
                 <Exo2/>
-                /*
+                */
                 <Exo3/>
+                /*
                 <Exo4/> 
                 */
             }
--- my-app/src/exo3/Exo3.jsx
+++ my-app/src/exo3/Exo3.jsx
@@ -1,12 +1,46 @@
 import React from 'react'
 import { TextField, Button } from '@material-ui/core'
 import "./exo3.css"
+import { useState, useEffect } from 'react'
 
 const KEY_LOCAL_STORAGE = 'my_title'
 
+const defaultValue = {
+    TextField : ''
+}
+
 const Exo3 = () => {
+    const [inputValue, setValues] = useState(defaultValue);
+
+
+    useEffect( () => {
+        const textFieldFromStorage = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE))
+        if(textFieldFromStorage){
+
+        setValues(textFieldFromStorage)
+        }
+       
+    }, [])
+
+
+   const saveMe = () => {
+       localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(inputValue))
+   }
+
+   const changeInputValue = event => {
+       setValues({TextField: event.target.value})
+   };
+
+
+
     return (
         <div className={"exo3"}>
+            <div>
+            <form   autoComplete="off">
+             <TextField onChange={changeInputValue} id="standard-basic" label="Standard" />
+            </form>
+            <Button onClick={saveMe} variant="contained" color="primary">Save</Button>
+            </div>
         </div>
     )
 }
