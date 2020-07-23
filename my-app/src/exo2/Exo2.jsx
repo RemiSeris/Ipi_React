088e20cc75bc6c9ba96ab286fed6f76b7f6063b8
Sileina <49021819+Sileina@users.noreply.github.com>
2020-07-23 14:34:38 +0200

    Exo 2

--- my-app/src/exo2/Exo2.jsx
+++ my-app/src/exo2/Exo2.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./exo2.css"
+import List from './components/List'
+import Item from './components/Item'
 
 const lists = [
     {
@@ -39,6 +41,14 @@ const lists = [
 const Exo2 = () => {
     return (
         <div className={"exo2"}>
+            {lists.map(({ items, title }, index) => {
+                    return <List title={title} key={index}>
+                        {items.map(({ title: itemTitle }, indexItem) => {
+                            return <Item title={itemTitle} key={index + '-' + indexItem} ></Item>
+                        })}
+                    </List>
+            })
+            }
         </div>
     )
 }
---
+++ my-app/src/exo2/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const List = ({ children, title }) => {
+    return (
+        <div className={'list '}>
+            <h3>{title}</h3>
+            {children}
+        </div>
+    )
+}
+
+export default List
