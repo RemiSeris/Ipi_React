9e887b7575b3d47beb09309d8ee804ff0a3a8e28
Sileina <49021819+Sileina@users.noreply.github.com>
2020-07-23 15:38:10 +0200

    Exo5

--- my-react-native-app/exo5/Exo5.jsx
+++ my-react-native-app/exo5/Exo5.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
 
 const Exo5 = () => {
-
+    const [isConnect, setIsConnect] = useState(false)
+    const [isConnectText, setIsConnectText] = useState('I am disconnected')
+    const onPressChangeState = () => {
+        setIsConnect(!isConnect)
+        isConnect? setIsConnectText('I am connected') : setIsConnectText('I am disconnected')
+    }
     return (
         <View >
-
+            <Text>{isConnectText}</Text>
+            <Button title={'Connect/Disconnect'} onPress={onPressChangeState}></Button>
         </View>
     );
 }
