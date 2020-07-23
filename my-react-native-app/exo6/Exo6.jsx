4adc0a6157245f3940e1065078cb056ecbaf656d
remi seris <remiseris@gmail.com>
2020-07-23 16:24:59 +0200

    modif

---
+++ my-react-native-app/exo6/Exo6.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const messageList = [
+    {
+        from: 'Pierre',
+        content: 'Salut, ça va ?'
+    },
+    {
+        from: 'François',
+        content: 'ça va et toi ?'
+    },
+    {
+        from: 'Pierre',
+        content: 'Trés bien'
+    },
+    {
+        from: 'François',
+        content: 'Ok, tu viens toujours à la pêche avec Martine?'
+    },
+]
+
+const Exo6 = () => {
+
+    return (
+        <View >
+
+        </View>
+    );
+}
+
+
+
+export default Exo6
