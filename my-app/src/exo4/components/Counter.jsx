ca07c6cefc6ed7aa16cc1aa4ad439b94697e9d88
André Remuzon <you@example.com>
2020-07-23 16:26:30 +0200

    exo2

---
+++ my-app/src/exo4/components/Counter.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { CounterContext } from '../CounterContextProvider'
+
+const Counter = () => {
+
+    const {nb} = useContext(CounterContext)
+
+
+    return (
+        <div className='nb'>
+            {nb}
+        </div>
+    )
+}
+
+
+export default Counter
