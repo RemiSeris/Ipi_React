c63945e24b7b5a583440bdc22df1b9970030d76a
NYAZAK <zakaria.nya@hetic.net>
2020-07-23 16:30:51 +0200

    exo 5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
  // import Exo1 from './exo1/Exo1'
 // import Exo2 from './exo2/Exo2'
- import Exo3 from './exo3/Exo3'
+ //import Exo3 from './exo3/Exo3'
 // import Exo4 from './exo4/Exo4'
 
 const App = () => {
@@ -13,7 +13,7 @@ const App = () => {
                 
              //    <Exo1/> 
             // <Exo2/>
-                <Exo3/>
+            //    <Exo3/>
               //  <Exo4/> 
                 
             }
--- my-react-native-app/exo5/Exo5.jsx
+++ my-react-native-app/exo5/Exo5.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
 
 const Exo5 = () => {
 
+    const [isConnected, setIsConnected] = useState(false);
+    
+    const switchTheButton = () => {
+        if(isConnected === true) {
+            setIsConnected(false)
+        } else {
+            setIsConnected(true)
+        }
+    }
+
+    const WhatAmI = isConnected ? 'isConnected' : "I'm disconnected";
+
     return (
         <View >
-
+<Button
+  onPress={switchTheButton}
+  title="connect/disconnect'"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
+    <Text>{WhatAmI}</Text>
         </View>
     );
 }
