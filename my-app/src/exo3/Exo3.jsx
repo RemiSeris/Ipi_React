51ccb16b8d303db71085519225a1d9b0160921c6
NYAZAK <zakaria.nya@hetic.net>
2020-07-23 15:28:49 +0200

    exo3 part 1

--- my-app/src/exo3/Exo3.jsx
+++ my-app/src/exo3/Exo3.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { TextField, Button } from '@material-ui/core'
 import "./exo3.css"
+import { useState } from 'react'
 
 const KEY_LOCAL_STORAGE = 'my_title'
 
 const Exo3 = () => {
+    const [inputValue] = useState('');
+
+
+
     return (
         <div className={"exo3"}>
+            <div className="saveMe">
+            <form   autoComplete="off">
+             <TextField id="standard-basic" label="Standard" />
+            </form>
+            <Button variant="contained" color="primary">Save</Button>
+            </div>
         </div>
     )
 }
