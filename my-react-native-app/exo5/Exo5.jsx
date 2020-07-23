2f440dfa3554056263826214c6568da3816cba9a
Meital <to31@hotmail.fr>
2020-07-23 16:33:59 +0200

    ex 5

--- my-react-native-app/exo5/Exo5.jsx
+++ my-react-native-app/exo5/Exo5.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
 
 const Exo5 = () => {
-
+    const [isConnect, setIsConnect] = useState(false);
+    const changeState = () => {
+        setIsConnect(!isConnect);
+    };
     return (
         <View >
-
+            <Text>{isConnect ? 'Connected':'Disconnected'}</Text>
+            <Button title={isConnect ? 'Connected':'Disconnected'} type="outline" onPress={changeState}/>
         </View>
     );
-}
+};
 
 
 
