8fe715609a7e96417ea2647b018efc46d415cd57
bastienlarrouture <44396321+bastienlarrouture@users.noreply.github.com>
2020-07-23 16:07:15 +0200

    Exo5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,19 @@
 import React from 'react';
 // import Exo1 from './exo1/Exo1'
 // import Exo2 from './exo2/Exo2'
-import Exo3 from './exo3/Exo3'
-// import Exo4 from './exo4/Exo4'
+// import Exo3 from './exo3/Exo3'
+import Exo4 from './exo4/Exo4'
 
 const App = () => {
 
     return (
         <div className='layout'>
             {
-                <Exo3/>
+                <Exo4/>
                 /*
                 <Exo1/>
                 <Exo2/>
-                <Exo4/> 
+                <Exo3/> 
                 */
             }
         </div>
--- my-react-native-app/exo5/Exo5.jsx
+++ my-react-native-app/exo5/Exo5.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Text } from 'react-native';
 
 const Exo5 = () => {
-
+    const [isConnect, setIsConnect] = useState(false)
+    const [isConnectText, setIsConnectText] = useState('disconnected')
+    const changeState = () => {
+        setIsConnect(!isConnect)
+        isConnect? setIsConnectText('connected') : setIsConnectText('disconnected')
+        console.log(isConnect)
+    }
     return (
         <View >
-
+            <Text>{isConnectText}</Text>
+            <Button title={'Connected?'} onPress={changeState}></Button>
         </View>
     );
 }
