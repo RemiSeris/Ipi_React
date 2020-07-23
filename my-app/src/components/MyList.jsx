5b077cc6c6524ae75066f0917413a253e5a75b5d
Sileina <49021819+Sileina@users.noreply.github.com>
2020-07-23 14:08:38 +0200

    Exo 1

---
+++ my-app/src/components/MyList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+const MyList = () => {
+    const [myList, setMyList] = useState(lists)
+
+    useEffect(() => {
+
+    }, [myList])
+
+    return {
+
+    }
+}
+
+export default MyList
