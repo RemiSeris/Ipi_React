00e17592d98953732f253c90f4255c80ed76e54a
AlexiaT31 <alexia.teissier@campus-igs-toulouse.fr>
2020-07-23 16:13:35 +0200

    exo 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 //Lorsque on veut créer un component on importe React
 import React from 'react';
 //import Exo1 from './exo1/Exo1';
-import Exo2 from './exo2/Exo2'
-// import Exo3 from './exo3/Exo3'
+//import Exo2 from './exo2/Exo2'
+import Exo3 from './exo3/Exo3'
 // import Exo4 from './exo4/Exo4'
 
 const App = () => {
@@ -11,10 +11,9 @@ const App = () => {
         <div className='layout'>
             {
                 //<Exo1/>
-                <Exo2/>
-                /*<Exo3/>
-                <Exo4/> 
-                */
+                //<Exo2/>
+                <Exo3/>
+                //<Exo4/>
             }
         </div>
     )
--- my-app/src/exo3/Exo3.jsx
+++ my-app/src/exo3/Exo3.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { TextField, Button } from '@material-ui/core'
 import "./exo3.css"
 
+
 const KEY_LOCAL_STORAGE = 'my_title'
 
+
+
+
 const Exo3 = () => {
+    const [inputValue, setInputValue] = useState("this is textfield")
+
+    const save = () => {
+        
+     localStorage.setItem('KEY_LOCAL_STORAGE', JSON.stringify(inputValue))   
+    }
     return (
-        <div className={"exo3"}>
+        <div className="exo3">
+            <TextField label={inputValue}></TextField>
+            <Button onClick={save} title="save">change</Button>
         </div>
     )
 }
