2c663bc3faaf0f011c59233e81abe158525adf6c
Zein1 <delavaloire.clement@gmail.com>
2020-07-23 16:09:22 +0200

    exo 3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 //Lorsque on veut créer un component on importe React
 import React from 'react';
 //import Exo1 from './exo1/Exo1'
-import Exo2 from './exo2/Exo2'
-// import Exo3 from './exo3/Exo3'
+//import Exo2 from './exo2/Exo2'
+import Exo3 from './exo3/Exo3'
 // import Exo4 from './exo4/Exo4'
 
 const App = () => {
@@ -12,8 +12,8 @@ const App = () => {
             {
                 
                 //<Exo1/>
-                <Exo2/>
-                //<Exo3/>
+                //<Exo2/>
+                <Exo3/>
                 //<Exo4/> 
             }
         </div>
--- my-app/src/exo3/Exo3.jsx
+++ my-app/src/exo3/Exo3.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { TextField, Button } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
 import "./exo3.css"
+import { TextField, Button, InputAdornment } from '@material-ui/core'
 
 const KEY_LOCAL_STORAGE = 'my_title'
 
 const Exo3 = () => {
+
+    const [inputValue, changeInput] = useState('')
+    const changeInputOnChange = (event) => { changeInput(event.target.value) }
+    const saveInput = () =>{ localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(inputValue)) }
+
+    useEffect(() => {
+        let loadedInput = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE))
+        if (loadedInput !== undefined) 
+        {
+            if (loadedInput !== null) {
+                changeInput(loadedInput)
+            }
+        }
+    }, [])
+
     return (
         <div className={"exo3"}>
+            <TextField value={inputValue} onChange={changeInputOnChange} id="input" label="TextField" InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start" />
+                    ),
+                }}/>
+                <div className='button'>
+                <Button onClick={saveInput} variant="contained" color="primary" >Save</Button>
+                </div>
         </div>
     )
 }
-
 export default Exo3
