fdd2c12d0cad4c682442ffadedb3e73a35627f3a
sirsaumon <dragonkhanx@gmail.com>
2020-07-23 13:47:29 +0200

    commitfirst

--- my-react-native-app/App.js
+++ my-react-native-app/App.js
@@ -1,19 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-// import Exo5 from './exo5/Exo5'
-// import Exo6 from './exo6/EXo6'
+import React, { useContext, useState } from 'react';
+import { StyleSheet, Button, View, TextInput, FlatList, Text, TouchableHighlight } from 'react-native';
+import { AppContext } from './AppContextProvider';
+import List from './components/List';
+import Item from './components/Item';
+import Popup from './components/Popup';
+
 
 const App = () => {
+  const { myLists, removeList, setOpen } = useContext(AppContext)
+
+  const openModal = () => {
+    setOpen(true)
+  }
+
+
+  const renderList = ({ item }) => {
+    return <List list={item}>
+      {
+        item.items.map((itemObj) => <Item key={itemObj.title} item={itemObj} />)
+      }
+    </List>
+  }
+
+  const extractKeyFromList = (item) => {
+    return item.title
+  }
+
 
   return (
     <View style={styles.container}>
-      {
-        /*
-         <Exo5/>
-         <Exo6/>
-        */
-      }
+
+      <Popup />
+      <FlatList
+        data={myLists}
+        renderItem={renderList}
+        keyExtractor={extractKeyFromList}
+      />
+
+      <Button title={"Add List"} onPress={openModal} />
+      <Button onPress={removeList} title={"Delete List"} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -23,8 +50,12 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    backgroundColor: 'white'
   }
 });
 
