44f18cbc2a004490e18f9fcda1e5f04d16d841ed
NYAZAK <zakaria.nya@hetic.net>
2020-07-23 13:39:51 +0200

    essai

---
+++ my-app/src/componentstwo/Appz.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Components from './compo'
+
+const Appz = () => {
+    return (<div>
+        
+    </div>)
+}
---
+++ my-app/src/componentstwo/compo.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Components = ({props, name, children}) =>  {
+    const myMethode = () => {
+
+    }
+
+    return (
+        <div>
+            mon retour
+        </div>
+    )
+}
+
+export default Components;
+
