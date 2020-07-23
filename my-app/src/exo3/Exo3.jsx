269408f633301335009b21687c9b2de36231fc26
Zein1 <delavaloire.clement@gmail.com>
2020-07-23 14:07:49 +0200

    first real push

---
+++ my-app/src/exo3/Exo3.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TextField, Button } from '@material-ui/core'
+import "./exo3.css"
+
+const KEY_LOCAL_STORAGE = 'my_title'
+
+const Exo3 = () => {
+    return (
+        <div className={"exo3"}>
+        </div>
+    )
+}
+
+export default Exo3
---
+++ my-react-native-app/exo5/Exo5.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Exo5 = () => {
+
+    return (
+        <View >
+
+        </View>
+    );
+}
+
+
+
+export default Exo5
