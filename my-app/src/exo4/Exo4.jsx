f7ca3f019252399283b7d6b78900afd3ac75efbf
remi seris <remiseris@gmail.com>
2020-07-23 10:24:26 +0200

    wip

--- my-app/src/exo4/Exo4.jsx
+++ my-app/src/exo4/Exo4.jsx
@@ -3,13 +3,15 @@ import "./exo4.css"
 
 const Exo4 = () => {
     return (
-        <div className={"exo4"}>
-            <div className={"counterContainer"}>
-                <CounterMore />
-                <Counter />
-                <CounterLess />
+        <CounterContextProvider>
+            <div className={"exo4"}>
+                <div className={"counterContainer"}>
+                    <CounterMore />
+                    <Counter />
+                    <CounterLess />
+                </div>
             </div>
-        </div>
+        </CounterContextProvider>
     )
 }
 
