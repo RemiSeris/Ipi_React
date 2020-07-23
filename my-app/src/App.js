8dedde364c7f5a34f8264d49c5a915b0369a37e5
Sileina <49021819+Sileina@users.noreply.github.com>
2020-07-23 15:21:56 +0200

    Exo 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import Exo1 from './exo1/Exo1'
 import Exo2 from './exo2/Exo2';
 import Exo3 from './exo3/Exo3';
+import CounterContextProvider from './exo4/CounterContextProvider';
 // import Exo2 from './exo2/Exo2'
 // import Exo3 from './exo3/Exo3'
-// import Exo4 from './exo4/Exo4'
+import Exo4 from './exo4/Exo4'
 
 const App = () => {
 
@@ -13,7 +14,7 @@ const App = () => {
         <div className='layout'>
             {
                 <Exo1></Exo1>
-                
+
                 /*
                  <Exo1/>
                 <Exo2/>
@@ -26,6 +27,9 @@ const App = () => {
             }
             {
                 <Exo3></Exo3>
+            }
+            {
+                    <Exo4></Exo4>
             }
         </div>
     )
