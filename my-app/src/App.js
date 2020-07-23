c2cf960b15d8bd32302d99fa120661bf6e84f33b
remi seris <remiseris@gmail.com>
2020-07-23 08:36:50 +0200

    wip

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+//Lorsque on veut créer un component on importe React
+import React from 'react';
+
+const App = () => {
+
+    return (
+        <div className='layout'>
+
+        </div>
+    )
+}
+
+
+export default App;
--- my-react-native-app/App.js
+++ my-react-native-app/App.js
@@ -1,41 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useContext, useState } from 'react';
-import { StyleSheet, Button, View, TextInput } from 'react-native';
-import { AppContext } from './AppContextProvider';
-import List from './components/List';
-import Item from './components/Item';
-import Popup from './components/Popup';
-
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 const App = () => {
-  const { myLists, removeList } = useContext(AppContext)
-  const [open, setOpen] = useState(false)
-  const [inputValue, setInputValue] = useState('')
-
-  const handleClose = () => {
-    setOpen(false)
-  }
-
-  const openModal = () => {
-    setOpen(true)
-  }
-
-  const onInputChange = (text) => {
-    setInputValue(text)
-  }
 
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input} value={inputValue} onChangeText={onInputChange} />
-      <Popup open={open} handleClose={handleClose} />
-      {
-        myLists.map((list) => <List list={list}>
-          {list.items.map((item) => <Item title={item.title} />)}
-        </List>
-        )}
-
-      <Button title={"Add List"} />
-      <Button onPress={removeList} title={"Delete List"} />
-      <Button title={"Open Modal"} onPress={openModal} />
 
       <StatusBar style="auto" />
     </View>
@@ -46,12 +15,8 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  input: {
-    backgroundColor: 'white'
   }
 });
 
