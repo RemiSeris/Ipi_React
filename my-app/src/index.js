2c386282f8b2bcdcea69f8d6b901b272edc5a5d3
José Rodrigues <jose1.rodrigues@epitech.eu>
2020-10-05 10:41:02 +0200

    init

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Layouts/App';
-import AppContextProvider from './AppContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppContextProvider>
       <App />
-    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
